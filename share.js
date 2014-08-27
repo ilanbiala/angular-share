03542f52dcdcff5525a03e998266a32277978562
Ilan Biala <ilan.biala@gmail.com>
2014-08-27 16:09:35 -0400

    Progress

--- share.js
+++ share.js
@@ -1,53 +1,90 @@
 angular.module('social-share', []);
 
-angular.module('social-share').directive('socialShare', [function() {
-  return {
-    restrict: 'AEC',
-    templateUrl: 'social-share.html',
-    compile: function(tElement, tAttrs, transclude) {
-      return {
-        pre: function($scope, iElement, iAttrs, controller) {
-          $scope.sharingMethods = Object.keys(iAttrs.$attr).map(function(method) {
-            method = method.toLowerCase();
-            return "twitter facebook google pinterest email".indexOf(method) > -1 ? method : null;
-          }).filter(function(method) {
-            return method != undefined;
-          });
-          console.log($scope.sharingMethods);
+angular.module('social-share').directive('socialShare', ['$window', 'Share',
+  function($window, Share) {
+    return {
+      restrict: 'AEC',
+      templateUrl: 'social-share.html',
+      link: function($scope, element, attrs) {
+        $scope.share = new Share({
+          direction: 'down',
+          style: 'flat'
+        });
+        $scope.$window = $window;
 
-          iElement.bind('click', function() {
-            debugger;
-          });
-        }
+        $scope.sharingUrls = {
+          twitter: encodeURI('https://twitter.com/intent/tweet?text=A simple, light, flexible, and good-looking share button&url=http://sharebutton.co/'),
+          facebook: encodeURI('https://www.facebook.com/dialog/share?app_id=184484190795&redirect_uri=http://google.com/'),
+          googleplus: encodeURI('https://plus.google.com/share?url=http://sharebutton.co/'),
+          email: encodeURI('mailto:?subject=Awesome share button built in angular&body=Look at this!'),
+        };
+
+        $scope.openShareLink = function(shareMethod) {
+          $window.open(
+            $scope.sharingUrls[shareMethod],
+            '_blank-' + shareMethod,
+            'height=' + $scope.share.options.height +
+            ',width=' + $scope.share.options.width +
+            ',left=' + ($window.innerWidth / 2 - $scope.share.options.width / 2) +
+            ',top=' + ($window.innerHeight / 2 - $scope.share.options.height / 2));
+        };
+
+        $scope.sharingMethods = Object.keys(attrs.$attr).map(function(method) {
+          method = method.toLowerCase();
+          if ('twitter facebook googleplus email'.indexOf(method) > -1) {
+            return {
+              name: method
+            };
+          }
+        }).filter(function(method) {
+          return method != undefined;
+        });
       }
-    },
-    controller: 'SocialShareController'
-  };
-}]).controller('SocialShareController', ['Share', function(Share) {
-
-}]).factory('Share', [function() {
-  function SocialShare(options) {
-    this.options = angular.extend({
-      twitter: true,
-      facebook: true,
-      google: true,
-      email: true,
-      direction: 'up'
-    }, options);
-
-    this.isOpen = false;
-    return this;
+    };
   }
+]).factory('Share', [
+  function() {
+    function SocialShare(options) {
+      this.options = angular.extend({
+        twitter: {
+          text: null,
+          url: null
+        },
+        facebook: {
+          url: null,
+          text: null
+        },
+        google: {
+          url: null,
+          text: null
+        },
+        email: {
+          url: null,
+          text: null
+        },
+        direction: 'up',
+        style: 'radius',
+        width: 575,
+        height: 400,
+        popupPosition: 'center'
+      }, options);
 
-  SocialShare.prototype.open = function() {
-    this.isOpen = true;
-    return this;
-  };
+      this.isOpen = false;
+      return this;
+    }
 
-  SocialShare.prototype.close = function() {
-    this.isOpen = false;
-    return this;
-  };
-}]);
+    SocialShare.prototype.open = function() {
+      this.isOpen = true;
+      return this;
+    };
+
+    SocialShare.prototype.close = function() {
+      this.isOpen = false;
+      return this;
+    };
+
+    return SocialShare;
+  }
+]);
 
 angular.module('myApp', ['social-share']);
