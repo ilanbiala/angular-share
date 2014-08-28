7103c1a31638dc300279cac75842964c4dec110f
Ilan Biala <ilan.biala@gmail.com>
2014-08-28 14:15:29 -0400

    Working!

--- share.js
+++ share.js
@@ -6,17 +6,41 @@ angular.module('social-share').directive('socialShare', ['$window', 'Share',
       restrict: 'AEC',
       templateUrl: 'social-share.html',
       link: function($scope, element, attrs) {
+        $scope.calculatePosition = function() {
+          return {
+            'left': document.querySelector('.social-share button').offsetLeft + document.querySelector('.social-share button').offsetWidth/2 - document.querySelector('.social-share ul').offsetWidth/2 + 'px',
+            'top': document.querySelector('.social-share button').offsetTop - 15 - document.querySelector('.social-share ul').offsetHeight + 'px'
+          };
+        };
+
         $scope.share = new Share({
+          twitter: {
+            url: 'http://ilanbiala.github.io/angular-share/',
+            text: 'Check out the new best AngularJS sharing plugin! Made with \u2764 by @ilanbiala'
+          },
+          facebook: {
+            redirectUrl: 'http://ilanbiala.github.io/angular-share/',
+            url: 'http://ilanbiala.github.io/angular-share/',
+            appId: 681511458592219
+          },
+          googleplus: {
+            url: 'http://ilanbiala.github.io/angular-share/'
+          },
+          email: {
+            url: 'http://ilanbiala.github.io/angular-share/',
+            text: 'AngularJS sharing plugin'
+          },
           direction: 'down',
+          position: $scope.calculatePosition,
           style: 'flat'
         });
         $scope.$window = $window;
 
         $scope.sharingUrls = {
-          twitter: encodeURI('https://twitter.com/intent/tweet?text=A simple, light, flexible, and good-looking share button&url=http://sharebutton.co/'),
-          facebook: encodeURI('https://www.facebook.com/dialog/share?app_id=681511458592219&display=popup&href=http://ilanbiala.github.io/angular-share/&redirect_uri=http://ilanbiala.github.io/angular-share/'),
-          googleplus: encodeURI('https://plus.google.com/share?url=http://sharebutton.co/'),
-          email: encodeURI('mailto:?subject=Awesome share button built in angular&body=Look at this!'),
+          twitter: encodeURI($scope.share.options.twitter.baseUrl + '?' + ($scope.share.options.twitter.url ? 'url=' + $scope.share.options.twitter.url : '') + ($scope.share.options.twitter.url && $scope.share.options.twitter.text ? '&text=' + $scope.share.options.twitter.text : '') + ($scope.share.options.twitter.text ? '&text=' + $scope.share.options.twitter.text : '')),
+          facebook: encodeURI($scope.share.options.facebook.baseUrl + '?app_id=' + $scope.share.options.facebook.appId + '&display=popup' + ($scope.share.options.facebook.url ? '&href=' + $scope.share.options.facebook.url : '') + '&redirect_uri=' + $scope.share.options.facebook.redirectUrl),
+          googleplus: encodeURI($scope.share.options.googleplus.baseUrl + '?' + ($scope.share.options.googleplus.url ? 'url=' + $scope.share.options.googleplus.url : '')),
+          email: encodeURI($scope.share.options.email.baseUrl + '?' + ($scope.share.options.email.text ? 'subject=' + $scope.share.options.email.text : '')),
         };
 
         $scope.openShareLink = function(shareMethod) {
@@ -31,9 +55,10 @@ angular.module('social-share').directive('socialShare', ['$window', 'Share',
 
         $scope.sharingMethods = Object.keys(attrs.$attr).map(function(method) {
           method = method.toLowerCase();
-          if ('twitter facebook googleplus email'.indexOf(method) > -1) {
+          if ('twitter facebook googleplus email'.indexOf(method) !== -1) {
             return {
-              name: method
+              name: method,
+              icon: $scope.share.options[method].icon
             };
           }
         }).filter(function(method) {
@@ -43,24 +68,45 @@ angular.module('social-share').directive('socialShare', ['$window', 'Share',
     };
   }
 ]).factory('Share', [
+
   function() {
     function SocialShare(options) {
-      this.options = angular.extend({
+      function deepExtend(destination, source) {
+        for (var property in source) {
+          if (source[property] && source[property].constructor &&
+            source[property].constructor === Object) {
+            destination[property] = destination[property] || {};
+            arguments.callee(destination[property], source[property]);
+          } else {
+            destination[property] = source[property];
+          }
+        }
+        return destination;
+      }
+
+      this.options = deepExtend({
         twitter: {
-          text: null,
-          url: null
+          baseUrl: 'https://twitter.com/intent/tweet',
+          url: '',
+          text: '',
+          icon: 'icon-twitter'
         },
         facebook: {
-          url: null,
-          text: null
+          baseUrl: 'https://www.facebook.com/dialog/share',
+          url: '',
+          icon: 'icon-facebook',
+          appId: null
         },
-        google: {
-          url: null,
-          text: null
+        googleplus: {
+          baseUrl: 'https://plus.google.com/share',
+          url: '',
+          icon: 'icon-googleplus'
         },
         email: {
-          url: null,
-          text: null
+          baseUrl: 'mailto:',
+          url: '',
+          text: '',
+          icon: 'icon-email'
         },
         direction: 'up',
         style: 'radius',
